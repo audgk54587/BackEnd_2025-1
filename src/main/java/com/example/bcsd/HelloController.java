e3b81dd0e10a5ab880c8730e85a79858e634fa61
myeongha <audgk54587@naver.com>
2025-05-06 13:20:45 +0900

    C:\Users\audgk\Downloads\demo\demo\src\BackEnd_2025-1\src\main\java\com\example\bcsd

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,4 +1,42 @@
-package com.example.bcsd;
+/*
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/introduce")
+    public String hello() {
+        return "안녕하세요 제 이름은 황명하입니다!";
+    }
+}*/
+
+
+/*
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/introduce")
+    public String hello(@RequestParam("name") String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
+    }
+}*/
+
+
+package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +46,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!!!!";
+    @GetMapping("/json")
+    public information jsonResponse() {
+        information data = new information();
+        data.Age(23);
+        data.Name("황명하");
+        return data;
+    }
+}
+
+class information {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
     }
 
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "hello";
+    public void Age(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void Name(String name) {
+        this.name = name;
     }
 }
